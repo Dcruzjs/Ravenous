fb1138b13216fc3d283f341b1ca35733bb92846f
Daniel Cruz <dcruz.js@gmail.com>
2020-05-30 08:36:58 -0600

    I add the interaction with Yelp API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from '../../logo.svg';
+/* import logo from '../../logo.svg'; */
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
+import Yelp from '../../util/Yelp'
 
-const business = {
+/* const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
@@ -14,21 +15,30 @@ const business = {
   category: 'Italian',
   rating: 4.5,
   reviewCount: 90
-}
+} */
 
-const businessesArray =[ business, business, business, business, business, business,]
+/* const businessesArray =[ business, business, business, business, business, business,] */
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this)
+  }
 
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
+    /* console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`) */
+    Yelp.searchYelp(term, location, sortBy)
+      .then( businesses => this.setState({ businesses: businesses}))
   }
   render(){
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList businesses={businessesArray} /> 
+        <BusinessList businesses={this.state.businesses} /> 
       </div>
     );
   }
